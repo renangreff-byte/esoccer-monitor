a45b842d580d8088b1a0d15798e6f58b05e67ba1
renangreff-byte <renan.greff@hotmail.com>
2026-08-18 22:40:01 -0300

    config: conectar dashboard ao Supabase

---
+++ web/config.js
@@ -0,0 +1,4 @@
+window.ES_CONFIG = {
+  SUPABASE_URL: "https://pybojmdmilhbkbhntuaf.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5Ym9qbWRtaWxoYmtiaG50dWFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwOTgzMjUsImV4cCI6MjEwMjY3NDMyNX0.VMitMiyfKfM4pdb9d7CT7Pv_KZwl455KtRKZFH-5qP8"
+};
